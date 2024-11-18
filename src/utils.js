7458b9a96cb44f0dc7ec2c86d482c66ac89bcaf7
roibuzaglo202 <roi.buzaglo@evinced.com>
2024-11-18 18:01:55 +0200

    adding files to implement

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+/**
+ * Fetches data from a JSON file.
+ * @param {string} filePath - Path to the JSON file.
+ * @returns {Promise<Array>} Parsed JSON data.
+ */
+const fetchData = async (filePath) => {
+};
+
+/**
+ * Deduplicates an array of objects based on specified keys.
+ * @param {Array<Object>} data - The array of objects to deduplicate.
+ * @param {Array<string>} keys - The keys to use for deduplication.
+ * @returns {Array<Object>} The deduplicated array of objects.
+ */
+const deduplicateByKeys = (data, keys) => {
+};
+
+export { fetchData, deduplicateByKeys };
